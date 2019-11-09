a0c52bbb625f3e2d3a2a34adc276edbf48b1552e
Alice Freda <alicefreda@gmail.com>
2019-11-09 21:20:46 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,11 +1,23 @@
 class Artist
+  attr_accessor :name
   @@all = []
   def initialize(name)
     @name = name
     save
   end
+
+  def self.all
+    @@all
+  end
+
   def save
     @@all << self
   end
+
+  def new_song(name, genre)
+    Song.new(name, self, genre)
+  end
+
+
 
 end
