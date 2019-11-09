9460a050d36e0370535d2d6f0335fef348cf7d11
Alice Freda <alicefreda@gmail.com>
2019-11-09 21:33:36 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,20 @@
+class Doctor
+  attr_accessor :name, :patient, :Appointment
+  
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    save
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    @@all << self
+  end
+
+  
+end
